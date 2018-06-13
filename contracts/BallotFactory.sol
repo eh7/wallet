512f09991843f7b7abb6889ffe48dc963b53bc68
Gavin Francis <gav@eh7.co.uk>
2018-06-13 23:24:21 +0100

    BallotFactory contract updates

--- contracts/BallotFactory.sol
+++ contracts/BallotFactory.sol
@@ -4,21 +4,30 @@ import "./Ownable.sol";
 
 contract BallotFactory is Ownable {
 
-  event NewBallot(uint _ballotId, string _description, uint _ballotOwnerId, address _owner);
+  event NewBallot(uint _ballotId, uint _ballotOwnerId, string _ballotDescription, address _ballotOwner);
+  event NewOption(uint _ballotId, string _description, uint _optionId);
   event NewVote(uint _ballotId, uint _voteId);
-/*
-  struct Option {
+
+  struct Vote {
+    address voter;
     uint ballotId;
-    uint option;
+    uint optionId;
   }
-*/
+
+  struct Option {
+    string optionText;
+    uint ballotId;
+  }
+
   struct Ballot {
     string description;
     uint timestamp;
+//    Option[] options;
+//    Vote[] votes;
   }
 
   Ballot[] public ballots;
-//  Option[] public options;
+  Option[] public options;
   address[] public ballotOwners;
 
   constructor() public {
@@ -26,10 +35,15 @@ contract BallotFactory is Ownable {
 
   function newBallot(string _description) public {
     uint ballotOwnerId = ballotOwners.push(msg.sender) - 1;
-    uint id = ballots.push(Ballot(_description, block.timestamp)) - 1;
-    emit NewBallot(id, _description, ballotOwnerId, msg.sender);
+    uint ballotId      = ballots.push(Ballot(_description, block.timestamp)) - 1;
+    emit NewBallot(ballotId, ballotOwnerId, _description, msg.sender);
+  }
+
+  function addOption(string _optionText, uint _ballotId) public {
+    uint newOptionId = options.push(Option(_optionText, _ballotId)); 
+    emit NewOption(_ballotId, _optionText, newOptionId);
   }
   
-  function castVote() public {
-  }
+//  function castVote() public {
+//  }
 }
